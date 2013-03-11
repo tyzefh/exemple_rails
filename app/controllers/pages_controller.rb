288222d543378e7882958fa4a27f54da8ea90c06
tyzef <tyzef@wendsonge.(none)>
2013-03-11 01:12:53 +0100

    Suppression de la page par défaut de rails

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,22 +4,18 @@ class PagesController < ApplicationController
   
   def home
     @titre="Accueil"
-    @base_title="Exemple - Tutoriel Ruby On Rails"
   end
 
   def contact
     @titre="Contact"
-    @base_title="Exemple - Tutoriel Ruby On Rails"
   end
   
   def about
     @titre="A propos"
-    @base_title="Exemple - Tutoriel Ruby On Rails"
   end
   
   def help
     @titre="Aide"
-    @base_title="Exemple - Tutoriel Ruby On Rails"
   end
   
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  
+  def titre
+    base_titre="Exemple - Tutoriel Ruby On Rails"
+    if @titre.nil?
+      base_titre
+    else
+      "#{base_titre} | #{@titre}"
+    end
+  end
+  
 end
